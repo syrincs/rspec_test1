c86aabec004484d66fe166dbe7ee0e2686094a17
André <andre@slimbytes.nl>
2019-08-09 01:09:18 +0200

    initial commit

---
+++ spec/hello_moppie_spec.rb
@@ -0,0 +1,127 @@
+require "rails_helper"
+
+class HelloMoppie
+  def say_hello
+    "Hello Moppie!"    
+  end  
+end
+
+describe HelloMoppie do
+  context "when testing the HelloMoppie class" do
+    it "should return 'Hello Moppie!' when we call the say_hello method" do
+      hm = HelloMoppie.new
+      message = hm.say_hello
+      expect(message).to eq "Hello Moppie!"
+    end
+  end
+end
+
+# Doubles
+class ClassRoom 
+  def initialize(students) 
+     @students = students 
+  end
+  
+  def list_student_names 
+     @students.map(&:name).join(',') 
+  end 
+end
+
+describe ClassRoom do
+  it 'the list_student_names method should work correctly' do 
+     student1 = double('student') 
+     student2 = double('student') 
+     
+     allow(student1).to receive(:name) { 'John Smith'} 
+     allow(student2).to receive(:name) { 'Jill Smith'} 
+     
+     cr = ClassRoom.new [student1,student2]
+     expect(cr.list_student_names).to eq('John Smith,Jill Smith') 
+  end 
+
+  # Hooks
+  class SimpleClass 
+    attr_accessor :message 
+    
+    def initialize() 
+       puts "\nCreating a new instance of the SimpleClass class" 
+       @message = 'howdy' 
+    end 
+    
+    def update_message(new_message) 
+       @message = new_message 
+    end 
+ end 
+ 
+ describe SimpleClass do 
+    #before(:all), after(:all), after(:each)
+    before(:each) do 
+       @simple_class = SimpleClass.new 
+    end 
+    
+    it 'should have an initial message' do 
+       expect(@simple_class).to_not be_nil
+       @simple_class.message = 'Something else. . .' 
+    end 
+    
+    it 'should be able to change its message' do
+       @simple_class.update_message('a new message')
+       expect(@simple_class.message).to_not be 'howdy' 
+    end
+ end
+end
+
+# Subjects
+class Person 
+  attr_reader :first_name, :last_name 
+  
+  def initialize(first_name, last_name) 
+     @first_name = first_name 
+     @last_name = last_name 
+  end 
+end 
+
+describe Person do 
+  it 'create a new person with a first and last name' do
+     person = Person.new 'John', 'Smith'
+     
+     expect(person).to have_attributes(first_name: 'John') 
+     expect(person).to have_attributes(last_name: 'Smith') 
+  end 
+end
+
+# Helpers
+class Dog
+  attr_reader :good_dog, :has_been_walked 
+  
+  def initialize(good_or_not)
+     @good_dog = good_or_not 
+     @has_been_walked = false 
+  end 
+  
+  def walk_dog 
+     @has_been_walked = true 
+  end 
+end 
+
+describe Dog do 
+  def create_and_walk_dog(good_or_bad)
+     dog = Dog.new(good_or_bad)
+     dog.walk_dog
+     return dog 
+  end 
+  
+  it 'should be able to create and walk a good dog' do
+     dog = create_and_walk_dog(true)
+     
+     expect(dog.good_dog).to be true
+     expect(dog.has_been_walked).to be true 
+  end 
+  
+  it 'should be able to create and walk a bad dog' do 
+     dog = create_and_walk_dog(false)
+     
+     expect(dog.good_dog).to be false
+     expect(dog.has_been_walked).to be true 
+  end 
+end
---
+++ spec/models/blog_spec.rb
@@ -0,0 +1,170 @@
+require 'rails_helper'
+
+RSpec.describe Blog, type: :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
+  describe "A test double" do
+    it "returns canned responses from the methods named in the provided hash" do
+      dbl = double("Some Collaborator", foo: 3, bar: 4)
+      expect(dbl.foo).to eq(3)
+      expect(dbl.bar).to eq(4)
+    end
+  end
+
+  describe "allow" do
+    it "returns nil from allowed messages" do
+      dbl = double("Some Collaborator")
+      allow(dbl).to receive(:foo)
+      expect(dbl.foo).to be_nil
+    end
+  end
+
+  describe "receive_messages" do
+    it "configures return values for the provided messages" do
+      dbl = double("Some Collaborator")
+      allow(dbl).to receive_messages(foo: 2, bar: 3)
+      expect(dbl.foo).to eq(2)
+      expect(dbl.bar).to eq(3)
+    end
+  end  
+
+  describe "A fulfilled positive message expectation" do
+    it "passes" do
+      dbl = double("Some Collaborator")
+      expect(dbl).to receive(:foo)
+      dbl.foo
+    end
+  end  
+
+  describe "A negative message expectation" do
+    it "passes if the message is never received" do
+      dbl = double("Some Collaborator").as_null_object
+      expect(dbl).not_to receive(:foo)
+    end
+  end
+
+  describe "A person" do
+    it "passes when the find method is called" do
+      person = double("person")
+      expect(person).to receive(:find) { person }
+      person.find
+    end
+  end
+
+  describe "A partial double" do
+    # Note: stubbing a string like this is a terrible idea.
+    #       This is just for demonstration purposes.
+    let(:string) { "a string" }
+    before { allow(string).to receive(:length).and_return(500) }
+  
+    it "redefines the specified methods" do
+      expect(string.length).to eq(500)
+    end
+  
+    it "does not effect other methods" do
+      expect(string.reverse).to eq("gnirts a")
+    end
+  end
+
+  # partial double
+  class User
+    def self.find(id)
+      :original_return_value
+    end
+  end
+  describe "A partial double" do
+    it "redefines a method" do
+      allow(User).to receive(:find).and_return(:redefined)
+      expect(User.find(3)).to eq(:redefined)
+    end
+  
+    it "restores the redefined method after the example completes" do
+      expect(User.find(3)).to eq(:original_return_value)
+    end
+  end  
+
+  # null_objects
+  describe "as_null_object" do
+    it "returns itself" do
+      dbl = double("Some Collaborator").as_null_object
+      expect(dbl.foo.bar.bazz).to be(dbl)
+    end
+  end  
+
+  describe "as_null_object / added method" do
+    it "can allow extra methods" do
+      dbl = double("Some Collaborator", foo: 3).as_null_object
+      allow(dbl).to receive(:bar).and_return(4)
+  
+      expect(dbl.foo).to eq(3)
+      expect(dbl.bar).to eq(4)
+    end
+  end
+
+  #spies
+  describe "have_received" do
+    it "passes when the message has been received" do
+      invitation = spy('invitation')
+      invitation.deliver
+      expect(invitation).to have_received(:deliver)
+    end
+  end  
+
+  class Invitation
+    def self.deliver; end
+  end
+  describe "have_received" do
+    it "passes when the expectation is met" do
+      allow(Invitation).to receive(:deliver)
+      Invitation.deliver
+      expect(Invitation).to have_received(:deliver)
+    end
+  end  
+
+  # describe "failure when the message has not been received" do
+  #   example "for a spy" do
+  #     invitation = spy('invitation')
+  #     #invitation.deliver
+  #     expect(invitation).to have_received(:deliver)
+  #   end
+  
+  #   example "for a partial double" do
+  #     allow(Invitation).to receive(:deliver)
+  #     #Invitation.deliver
+  #     expect(Invitation).to have_received(:deliver)
+  #   end
+  # end  
+
+  describe "An invitiation" do
+    let(:invitation) { spy("invitation") }
+  
+    before do
+      invitation.deliver("foo@example.com")
+      invitation.deliver("bar@example.com")
+    end
+  
+    it "passes when a count constraint is satisfied" do
+      expect(invitation).to have_received(:deliver).twice
+    end
+  
+    it "passes when an order constraint is satisfied" do
+      expect(invitation).to have_received(:deliver).with("foo@example.com").ordered
+      expect(invitation).to have_received(:deliver).with("bar@example.com").ordered
+    end
+  
+    # it "fails when a count constraint is not satisfied" do
+    #   # invitation.deliver("foo@example.com")
+    #   # invitation.deliver("foo@example.com")
+    #   # invitation.deliver("foo@example.com")
+    #   expect(invitation).to have_received(:deliver).at_least(3).times
+    # end
+  
+    # it "fails when an order constraint is not satisfied" do
+    #   expect(invitation).to have_received(:deliver).with("bar@example.com").ordered
+    #   expect(invitation).to have_received(:deliver).with("foo@example.com").ordered
+
+    #   # right order
+    #   # expect(invitation).to have_received(:deliver).with("foo@example.com").ordered
+    #   # expect(invitation).to have_received(:deliver).with("bar@example.com").ordered
+    # end
+  end
+end
