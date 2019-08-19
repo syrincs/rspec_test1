dece0d30bd59ad2d8c937607fcb2f76a59aac4c3
André <andre@slimbytes.nl>
2019-08-19 23:18:58 +0200

    added amount and author to blog

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -69,6 +69,6 @@ class BlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:name, :description)
+      params.require(:blog).permit(:name, :description, :amount, :author)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_27_210354) do
+ActiveRecord::Schema.define(version: 2019_08_19_105725) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "amount"
+    t.string "author"
   end
 
 end
